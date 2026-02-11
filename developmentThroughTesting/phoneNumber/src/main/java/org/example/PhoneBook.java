3010275ed6b26ff8f5869af3550db4ddc32ecb35
Aleksandr <user.kin@mail.ru>
2026-02-11 19:53:42 +0300

    Stub and tests for add

--- developmentThroughTesting/phoneNumber/src/main/java/org/example/PhoneBook.java
+++ developmentThroughTesting/phoneNumber/src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+    public int add(String name, int number){
+        return 0;
+    }
+
 }
--- developmentThroughTesting/phoneNumber/src/test/java/PhoneBookTest.java
+++ developmentThroughTesting/phoneNumber/src/test/java/PhoneBookTest.java
@@ -1,2 +1,14 @@
+import org.example.PhoneBook;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
+
+    @Test
+    public void returnOne() {
+        PhoneBook phoneBook = new PhoneBook();
+        int res = phoneBook.add("Jim", 8800);
+        assertEquals(1, res);
+    }
 }
