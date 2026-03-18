e993b81f5c2f569d4266c93fd77602de433d0d8a
Aleksandr <user.kin@mail.ru>
2026-03-18 22:43:30 +0300

    Replace manual DI with Spring annotation config

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -8,14 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.SpringConfig;
+
 public class MainServlet extends HttpServlet {
   private PostController controller;
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+    controller = context.getBean(PostController.class);
   }
 
   @Override
